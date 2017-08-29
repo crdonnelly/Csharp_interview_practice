30a2d9791051e0c6b251930d6f97e4a5a443dac4
Cameron Donnelly <cr.donnelly@hotmail.com>
2017-08-29 16:01:42 +1200

    Modified the arrays.cs class and compiled the arrays.exe

--- arrays.cs
+++ arrays.cs
@@ -1,8 +1,21 @@
 //arrays.cs
 using System;
+using System.Collections;
 class PracticeArrays
 {
-  public static void Main()
+  public static void Main(string[] arg1)
+  {
+    if (arg1[0] == "example")
+    {
+      example();
+    }
+    else if (arg1[0] == "single")
+    {
+      single();
+    }
+  }
+
+  public static void example()
   {
     //Single-dimensional array
     int[] numbers = new int[5];
@@ -13,10 +26,21 @@ class PracticeArrays
     //Array-of-arrays
     byte[][] scores = new byte[5][];
 
-    //Create jagged array
+    //Create the jagged array
+    for (int i = 0; i < scores.Length; i++)
+    {
+      scores[i] = new byte[i+3];
+    }
+
+    //Print the Length of each row
     for (int i = 0; i < scores.Length; i++)
     {
       Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
     }
+  }
+
+  public static void single()
+  {
+
   }
 }
